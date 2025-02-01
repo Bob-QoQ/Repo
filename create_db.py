a56e58793717692779af48699619431230b1dcd5
Bob-QoQ <itisveautiful_2@hotmail.com>
2025-02-02 01:58:30 +0800

    111222333

--- create_db.py
+++ create_db.py
@@ -63,7 +63,7 @@ def import_data():
     create_tables(conn)
     
     # 匯入大樂透資料
-    with open('Lotto-Crawler/data/BigLotto.json', 'r', encoding='utf-8') as f:
+    with open('data/BigLotto.json', 'r', encoding='utf-8') as f:
         big_lotto_data = json.load(f)
         cursor = conn.cursor()
         # 遍歷字典中的每個值
@@ -81,7 +81,7 @@ def import_data():
             ))
     
     # 匯入威力彩資料
-    with open('Lotto-Crawler/data/SuperLotto.json', 'r', encoding='utf-8') as f:
+    with open('data/SuperLotto.json', 'r', encoding='utf-8') as f:
         super_lotto_data = json.load(f)
         for item in super_lotto_data.values():  # 修改這裡
             nums = item['draw_order_nums']
@@ -97,7 +97,7 @@ def import_data():
             ))
     
     # 修改今彩539資料导入逻辑
-    with open('Lotto-Crawler/data/DailyCash.json', 'r', encoding='utf-8') as f:
+    with open('data/DailyCash.json', 'r', encoding='utf-8') as f:
         daily_cash_data = json.load(f)
         for item in daily_cash_data.values():
             nums = item['draw_order_nums'][:5]  # 只取前5个号码
